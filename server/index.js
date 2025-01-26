84fa9b26d68616ba3bad2d48af529e9d71b64b31
Salmon Joy <salmonjoy1996@gmail.com>
2025-01-26 18:34:37 +0530

    dotenv setup with gitignore

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { configDotenv } from "dotenv";
 
 const app = express()
+configDotenv()
+
 
 app.get("/",(req,res)=>{
     res.send("Welcome")
+    console.log(process.env.OPENAI_API)
 })
 
 app.listen(3000,(req,res)=>{
